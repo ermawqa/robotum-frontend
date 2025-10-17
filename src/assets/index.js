7fe300e1924a42dcce4506dab86543499066b833
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-10-17 19:04:50 +0200

    pass

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,6 +14,8 @@ export { default as ansys } from './images/partners/ansys.png'
 export { default as cubemars } from './images/partners/cubemars.png'
 export { default as maytec } from './images/partners/maytec.png'
 export { default as maxon } from './images/partners/maxon.png'
+export { default as reply } from './images/partners/roboverse-reply.svg'
+export { default as limx } from './images/partners/limx-dynamics.jpg'
 
 // Projects
 export { default as humanoidImg } from './images/projects/humanoid-project.png'
